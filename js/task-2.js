ecca3e69c383d97241d5e0304983f5f9c9963f68
Ordinateur 1 <abcvika3@gmail.com>
2026-01-09 20:59:06 +0100

    add corrections

--- js/task-2.js
+++ js/task-2.js
@@ -1,10 +1,8 @@
 function getShippingMessage (country, price, deliveryFee) {
   const totalPrice = price + deliveryFee;
   
-  console.log(`Shipping to ${country} will cost ${totalPrice} credits`);
+  return "Shipping to " + country + " will cost "  + totalPrice + " credits";
 }
-
-
 
 console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,10 @@
 function getElementWidth(content, padding, border) {
-console.log(parseFloat(content) + (parseFloat(padding) * 2) + (parseFloat(border) * 2));
+  const contentNum = Number.parseFloat(content);
+  const paddingNum =Number.parseFloat(padding);
+  const borderNum = Number.parseFloat(border);
+  return contentNum + paddingNum * 2 + borderNum * 2;
 }
-
+ 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
